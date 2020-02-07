61e9f24de14230786e28583a7ccf702cf1a7b448
Menny Even Danan <menny@evendanan.net>
2020-02-07 13:30:40 -0500

    Fix deploy-reports folder

--- .github/actions/deploy-request/request.sh
+++ .github/actions/deploy-request/request.sh
@@ -8,9 +8,8 @@ API_USERNAME="${2}"
 API_TOKEN="${3}"
 OUTPUT="${4}"
 
-TEMP_FOLDER="/tmp/${OUTPUT}"
-rm -rf "${TEMP_FOLDER}" || true
-mkdir -p "${TEMP_FOLDER}"
+rm -rf "${OUTPUT}" || true
+mkdir -p "${OUTPUT}"
 
 function deployment_request() {
   echo "making request to: ${1}"
@@ -21,10 +20,12 @@ function deployment_request() {
                     --arg jsonDescription "${2}" \
                     '{ ref: $jsonRef, task: "deploy", auto_merge: false, environment: $jsonDeployTarget, description: $jsonDescription, required_contexts: [ "push-ready" ] }' )
 
-  local JSON_FILENAME="${TEMP_FOLDER}/deployment_request_${1}.json"
+  local JSON_FILENAME="${OUTPUT}/deployment_request_${1}.json"
   echo "${JSON_TEXT}" > "${JSON_FILENAME}"
-
-  curl -u "${API_USERNAME}:${API_TOKEN}" -o "${TEMP_FOLDER}/deployment_response_${1}.json" -d "@${JSON_FILENAME}" https://api.github.com/repos/AnySoftKeyboard/AnySoftKeyboard/deployments
+  cat "${JSON_FILENAME}"
+  curl -u "${API_USERNAME}:${API_TOKEN}" -o "${OUTPUT}/deployment_response_${1}.json" -d "@${JSON_FILENAME}" https://api.github.com/repos/AnySoftKeyboard/AnySoftKeyboard/deployments
+  echo "response:"
+  cat "${OUTPUT}/deployment_response_${1}.json"
 }
 
 #some deploy logic
@@ -34,8 +35,3 @@ if [[ "${REF_TO_DEPLOY}" == "refs/heads/master" ]]; then
 elif [[ "${REF_TO_DEPLOY}" == "release-branch-v"* ]]; then
   deployment_request "app_beta" "Deployment request by ${API_USERNAME}"
 fi
-
-rm -rf "${OUTPUT}" || true
-mkdir -p "${OUTPUT}"
-
-mv "${TEMP_FOLDER}" "${OUTPUT}"
